1ec26423b2fde36c051c0eed3da6d765a6d9f8b9
Neha Bhavsar <bhavsarneha21@gmail.com>
2024-08-31 19:24:07 +0530

    initial change

--- src/routes/file.routes.ts
+++ src/routes/file.routes.ts
@@ -5,7 +5,7 @@ import { verifyToken } from '../middleware/verifyToken';
 
 const FileRouter: Router = express.Router();
 
-FileRouter.post('/upload', verifyToken, upload.single("file"), uploadFile);
-FileRouter.get('/:filename', verifyToken, getFile);
+// FileRouter.post('/upload', verifyToken, upload.single("file"), uploadFile);
+// FileRouter.get('/:filename', verifyToken, getFile);
 
 export default FileRouter;
